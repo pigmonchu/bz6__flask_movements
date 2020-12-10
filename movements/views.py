08aca55d5afd3d0669ebe05cfb0658dd00ef0f32
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-12-10 23:47:59 +0100

    insert into movimientos

--- movements/views.py
+++ movements/views.py
@@ -22,14 +22,33 @@ def listaIngresos():
     for ingreso in ingresos:
         total += float(ingreso[2])
 
+    conn.close()
+
     return render_template("movementsList.html",datos=ingresos, total=total)
 
 @app.route('/creaalta', methods=['GET', 'POST'])
 def nuevoIngreso():
     if request.method == 'POST':
+        # iNSERT INTO movimientos (cantidad, concepto, fecha) VALUES (1500, "Paga extra", "2020-12-16" )
+
+        conn = sqlite3.connect('movements/data/basededatos.db')
+        c = conn.cursor()
+
+        c.execute('INSERT INTO movimientos (cantidad, concepto, fecha) VALUES (?, ? ,? );', 
+                 (
+                    float(request.form.get('cantidad')),
+                    request.form.get('concepto'),
+                    request.form.get('fecha')
+                 )
+        )
+
+        conn.commit()
+        conn.close()
+        '''   
         fIngresos = open("movements/data/basededatos.csv", "a", newline="")
         csvWriter = csv.writer(fIngresos, delimiter=',', quotechar='"')
         csvWriter.writerow([request.form.get('fecha'), request.form.get('concepto'), request.form.get('cantidad')])
+        '''
         return redirect(url_for('listaIngresos'))
         
 
